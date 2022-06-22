a4b7f033b5f9c217e646d08060c70dbc3b201dcb
MirlanTerenov <kakamern@mail.ru>
2022-06-22 22:27:00 +0600

    Создал модель и миграцию в таблицу

--- git_practice/webapp/models.py
+++ git_practice/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок")
+    author = models.CharField(max_length=50, verbose_name="Заголовок", default="Unknown")
+    content = models.TextField(max_length=2000, verbose_name="Контент")
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.id}. {self.title} : {self.author}"
+
+    class Meta:
+        db_table = "articles"
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+
--- git_practice/webapp/views.py
+++ git_practice/webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(requeste):
+    if requeste.method == "GET":
+        return render(requeste, 'index.html')
+    else:
+        contex = {
+            'title': requeste.POST.get("title"),
+            'author': requeste.POST.get("author"),
+            'content': requeste.POST.get("content")
+
+        }
+        return render(requeste, 'index.html')
