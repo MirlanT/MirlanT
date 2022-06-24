bc0357b3003bc455af9570a2c177de1e4d75b7a3
MirlanTerenov <kakamern@mail.ru>
2022-06-24 13:21:30 +0600

    Реализация статуса

--- git_practice/webapp/models.py
+++ git_practice/webapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок")
@@ -8,6 +8,8 @@ class Article(models.Model):
     content = models.TextField(max_length=2000, verbose_name="Контент")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    status = models.CharField(max_length=20, choices=status_choices, default=status_choices[0][0], verbose_name="Статус")
+
 
     def __str__(self):
         return f"{self.id}. {self.title} : {self.author}"
