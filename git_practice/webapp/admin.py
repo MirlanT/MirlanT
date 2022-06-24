05c2b8a1cdfafffeeb7832ca295023c4c799a4df
MirlanTerenov <kakamern@mail.ru>
2022-06-24 15:35:55 +0600

    Добавление времени

--- git_practice/webapp/admin.py
+++ git_practice/webapp/admin.py
@@ -4,12 +4,12 @@ from webapp.models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'author', 'created_at']
+    list_display = ['id', 'title', 'author', 'date']
     list_display_links = ['title']
     list_filter = ['author']
     search_fields = ['title', 'content']
-    fields = ['title', 'author', 'content', 'created_at', 'updated_at']
-    readonly_fields = ['created_at', 'updated_at']
+    fields = ['title', 'author', 'content', 'date']
+    readonly_fields = ['created_at', 'date']
 
 
 admin.site.register(Article, ArticleAdmin)
--- git_practice/webapp/models.py
+++ git_practice/webapp/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+STATUS_CH = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок")
     author = models.CharField(max_length=50, verbose_name="Автор", default="Unknown")
     content = models.TextField(max_length=2000, verbose_name="Контент")
+    date = models.DateField(max_length=30, null=True, blank=True, verbose_name="Дата")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
-    status = models.CharField(max_length=20, choices=status_choices, default=status_choices[0][0], verbose_name="Статус")
+    status = models.CharField(max_length=20, choices=STATUS_CH, default=STATUS_CH[0][0], verbose_name="Статус")
 
 
     def __str__(self):
